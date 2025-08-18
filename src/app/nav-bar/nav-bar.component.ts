137057abab5a4e354c9ff087bda1bd18f5ee783e
John <johnpaulfrancis@gmail.com>
2025-08-19 05:22:39 +0530

    Visibility of abutton in nav-bar displayed based on if user is loggined or not

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+import { AuthServiceService } from '../services/auth-service.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [RouterLink],
+  imports: [RouterLink, CommonModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.css'
 })
-export class NavBarComponent {
 
+export class NavBarComponent implements OnInit {
+
+  isLoggedIn = false;
+  userName: string | null = null;
+
+  constructor(private authService: AuthServiceService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.checkLogin();
+  }
+
+  checkLogin() {
+    if (this.authService.isLoggedIn()) {
+      this.isLoggedIn = true;
+    }
+    else {
+      this.isLoggedIn = false;
+    }
+  }
 }
